c4898e9b47c2597233d89907e647cbd9a4ad8993
Haverra <r0464303@student.thomasmore.be>
2015-08-24 18:25:22 +0200

    Admin

---
+++ src/IntoPeople/AdminBundle/Form/OrganizationType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace IntoPeople\AdminBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class OrganizationType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'IntoPeople\AdminBundle\Entity\Organization'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'intopeople_adminbundle_organization';
+    }
+}
