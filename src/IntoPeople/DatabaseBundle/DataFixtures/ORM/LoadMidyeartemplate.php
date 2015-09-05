2e16394bb09a1a5d02b98b1a4684eef1993bd722
Matthias <matthias.stala@gmail.com>
2015-09-05 16:53:51 +0200

    fixtures

--- src/IntoPeople/DatabaseBundle/DataFixtures/ORM/LoadMidyeartemplate.php
+++ src/IntoPeople/DatabaseBundle/DataFixtures/ORM/LoadMidyeartemplate.php
@@ -8,7 +8,7 @@ use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use IntoPeople\DatabaseBundle\Entity\Midyeartemplate;
-
+use Proxies\__CG__\IntoPeople\DatabaseBundle\Entity\Midyear;
 
 
 class LoadMidyeartemplate extends AbstractFixture implements OrderedFixtureInterface
@@ -17,35 +17,61 @@ class LoadMidyeartemplate extends AbstractFixture implements OrderedFixtureInter
      * {@inheritDoc}
      */
     public function load(ObjectManager $manager)
-    {       
+    {
         $midyeartemplate = new Midyeartemplate();
- 
-        $midyeartemplate->setBigTitle("Year End");
-        $midyeartemplate->setTitle1("Personalia");
-        $midyeartemplate->setTitle2("Mid year review on progression of set goals");
-        $midyeartemplate->setTitle3("Feedback for your supervisor");
-        $midyeartemplate->setTitle4("Feedback for your organization");
-        $midyeartemplate->setSmallTitle1("Job Specific Tasks and Responsibilities");
-        $midyeartemplate->setSmallTitle2("Job Specific Skills and Competencies");
-        $midyeartemplate->setSmallTitle3("Move Intermodal Competencies");
-        
-        $midyeartemplate->setWhatwhy("What & why");
-        $midyeartemplate->setWhatwhydescription("Find stated objectives and motivation as agreed upon at the start of this year.");
-        $midyeartemplate->setProgress("Progression");
-        $midyeartemplate->setProgressdescription("How is it going?");
-        $midyeartemplate->setOrganizationfield("Feedback for your organization");
-        $midyeartemplate->setFeedbacksup("Feedback for your supervisor");
-        $midyeartemplate->setFeedbacksupdescription("Positive feedback or points of attention for your supervisor");
-        $midyeartemplate->setFeedbackorganizationdescription("Positive feedback and/or points of attention for your organization");
-        
-        $dt = new \DateTime();              
-        
-        $midyeartemplate->setDate($dt);
-        $midyeartemplate->setIsstandardtemplate(true);
+
+        $midyeartemplate->setMainTitle('Career Development Plan');
+        $midyeartemplate->setTitle1('Personalia');
+
+        $midyeartemplate->setName('Name');
+        $midyeartemplate->setSupervisor('Supervisor');
+        $midyeartemplate->setDateDiscussion('Date discussion');
+        $midyeartemplate->setTeam('Team');
+        $midyeartemplate->setFunction('Job Title');
+
+        $midyeartemplate->setTitle2("Self Assessment");
+        $midyeartemplate->setTitle2Description("Complete at least three, no more than five items for both strengths and development needs. ");
+
+        $midyeartemplate->setTable2Col1("Talents");
+        $midyeartemplate->setTable2Col2("Challenges");
+        $midyeartemplate->setTable2Col3("How");
+        $midyeartemplate->setTable2Col4("Success");
+        $midyeartemplate->setTable2Col5("Needs");
+
+        $midyeartemplate->setTable2Col1Descr("What are you good at?");
+        $midyeartemplate->setTable2Col2Descr("So what is your challenge?");
+        $midyeartemplate->setTable2Col3Descr("How are you going to take on this challenge?");
+        $midyeartemplate->setTable2Col4Descr("How will you know if you succeeded?");
+        $midyeartemplate->setTable2Col5Descr("What do you need to complete your challenge?");
+
+        $midyeartemplate->setTitle3('Development Needs for your Job');
+        $midyeartemplate->setTitle3Description('Read your Job Description carefully.');
+
+        $midyeartemplate->setTable3Col1("What & why");
+        $midyeartemplate->setTable3Col2("How");
+
+        $midyeartemplate->setTable3Col1Descr("Write down the actions you will take this year to further develop yourself in your job.");
+        $midyeartemplate->setTable3Col2Descr("How will you do this?");
+
+
+        $midyeartemplate->setTable3Title1('Job Specific Tasks and Responsibilities');
+        $midyeartemplate->setTable3Title2('Job Specific Skills and Competencies');
+        $midyeartemplate->setTable3Title3('Organization Competencies');
+
+        $midyeartemplate->setTitle4("Career Objectives / Staffing Considerations");
+        $midyeartemplate->setQuestion1("Where do you see yourself in three years?");
+        $midyeartemplate->setQuestion2("List any additional information that is relevant to your Career Development Plan (CDP) for the coming year. Such information could include your interest in special assignments, considerations related to work / life balance, etc.");
+
+        $midyeartemplate->setSupervisorComment("Supervisor Comments");
+        $midyeartemplate->setFeedback("Feedback BOD");
+        $midyeartemplate->setSignatureSupervisor("Date & supervisor's Signature");
+        $midyeartemplate->setSignatureEmployee("Date & Employee's Signature");
         $midyeartemplate->setLanguage($this->getReference('english'));
 
+        $dt = new \DateTime();
+        $midyeartemplate->setDate($dt);
+
         $midyeartemplate->setOrganization($this->getReference('organization'));
-        
 
         $manager->persist($midyeartemplate);
         $manager->flush();
